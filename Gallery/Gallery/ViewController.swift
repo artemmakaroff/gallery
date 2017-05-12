1d2e4bde98865321b97f4c6ec77e4aa15744e609
Artem Makarov <artemmakaroff@mail.ru>
2017-05-12 12:11:52 +0700

    Начал работу над приложением 12.05.17 в 12:10.

---
+++ Gallery/Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Artem Makarov on 12.05.17.
+//  Copyright © 2017 Artem Makarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
